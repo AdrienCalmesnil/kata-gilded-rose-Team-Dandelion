80514fbb2a61ee6a4f09233b15105d3ab4ce7718
Adrien Calmesnil <adcalmesnil@gmail.com>
2022-11-22 11:53:46 +0100

    2- refacto functions with conditions

--- javascript/src/gilded_rose.js
+++ javascript/src/gilded_rose.js
@@ -15,60 +15,89 @@ class Shop {
   /!\ Do not change code above this line /!\
   */
 
+  // changeQuality(id, changeValue) {
+  //   if (this.items[id].quality > 0 && this.items[id].quality < 50) {
+  //     this.items[id].quality = this.items[id].quality + changeValue;
+  //   }
+  // }
+
+  // checkItem() {
+  //   for (var i = 0; i < this.items.length; i++) {
+  //     switch (this.items[i].name) {
+  //       case "Sulfuras, Hand of Ragnaros":
+  //         console.log("This is a legendary Item ! Don't touch !");
+  //         break;
+  //       case "Aged Brie":
+  //         this.items[i].quality = this.items[i].quality - 2;
+
+  //       case "Backstage passes to a TAFKAL80ETC concert" :
+
+  //       default:
+  //         this.items[i].quality = this.items[i].quality - 1;
+  //     }
+  //   }
+  // }
+
+  isQualityIncrease(item) {
+    if (
+      item.name != "Aged Brie" &&
+      item.name != "Backstage passes to a TAFKAL80ETC concert"
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  qualityPositive(item) {
+    return item.quality > 0;
+  }
+
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
+    this.items.forEach((item) => {
+      if (this.isQualityIncrease(item)) {
+        if (this.qualityPositive(item)) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality = item.quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.sellIn = item.sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
+      if (item.sellIn < 0) {
+        if (item.name != "Aged Brie") {
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.quality > 0) {
+              if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.quality = item.quality - 1;
               }
             }
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality = item.quality - item.quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
       }
-    }
+    });
 
     return this.items;
   }
